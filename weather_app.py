02617c7abbeb99d87930c8cb100a7ef3b64726ec
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-31 10:26:01 +0800

    added the API details

--- weather_app.py
+++ weather_app.py
@@ -1,6 +1,6 @@
 # Weather Application
 
-import tkinter as tk
+import requests, tkinter as tk
 from tkinter import BOTH, IntVar
 
 # Define window
@@ -19,6 +19,52 @@ large_font = ("Simsun", 14)
 small_font = ("Simsun", 10)
 
 # Define functions
+def search():
+    """Use openweather api to look up current weather condition given a city/zip code"""
+    global response
+
+    # Get API response
+    # URL and api key
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    api_key = "59a99dfbbe0ddb794763ae17dcd255ab"
+
+    # Search by the appropriate query, either city name or zip
+    if search_method.get() == 1:
+        querystring = {"q": city_entry.get(), "appid": api_key}
+    elif search_method.get() == 2:
+        querystring = {"zip": city_entry.get(), "appid": api_key}
+
+    # Call API
+    response = requests.request("GET", url, params=querystring)
+    response = response.json()
+
+
+    # Example response return
+    """{'coord': {'lon': -71.0598, 'lat': 42.3584},
+     'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01n'}], 'base': 'stations',
+     'main': {'temp': 291.71, 'feels_like': 291.08, 'temp_min': 288.72, 'temp_max': 293.4, 'pressure': 1011,
+              'humidity': 56}, 'visibility': 10000, 'wind': {'speed': 2.68, 'deg': 333, 'gust': 7.15},
+     'clouds': {'all': 0}, 'dt': 1627697442,
+     'sys': {'type': 2, 'id': 2013408, 'country': 'US', 'sunrise': 1627637694, 'sunset': 1627689974},
+     'timezone': -14400, 'id': 4930956, 'name': 'Boston', 'cod': 200}
+    """
+    get_weather()
+
+def get_weather():
+    """Grab information from API response and update our weather labels."""
+    # Gather the data to be used from the API response
+    city_name = response['name']
+    city_lat = str(response['coord']['lat'])
+    city_lon = str(response['coord']['lon'])
+
+    main_waather = response['weather'][0]['main']
+    description = response['weather'][0]['description']
+
+    temp = str(response['main']['temp'])
+    feels_like = str(response['main']['feels_like'])
+    temp_min = str(response['main']['temp_min'])
+    temp_max = str(response['main']['temp_max'])
+    humidity = str(response['main']['humidity'])
 
 
 # GUI layout
@@ -56,7 +102,7 @@ photo_label.pack(pady=8)
 # Input frame layout
 # Create input frame buttons and entry
 city_entry = tk.Entry(input_frame, width=20, font=large_font)
-submit_button = tk.Button(input_frame, text="Submit", font=large_font, bg=input_color)
+submit_button = tk.Button(input_frame, text="Submit", font=large_font, bg=input_color, command=search)
 
 search_method = IntVar()
 search_method.set(1)
